8ac5b5c81c13ebead2dbdf114790c5e0a73468d3
Paul Clarke <paulioclarke@icloud.com>
2022-04-06 09:46:57 +1000

    Adds tests that WILL INTENTIONALLY FAIL

---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+import pytest
+from main import UQCSTalk
+
+INITIAL_DURATION = 1
+
+'''
+Defining the talk object to use for all of my tests
+'''
+@pytest.fixture
+def talk():
+    return UQCSTalk("An Introduction to the CI/CD Workflow with Paul Clarke", INITIAL_DURATION)
+
+def test_add_hour(talk: UQCSTalk):
+    talk.add_hour()
+    assert talk.get_hours() == "I can code HTML"
