c988978e30fcc28eb2de9aa3dc0b4bd6b35e2de2
Paul Clarke <paulioclarke@icloud.com>
2022-04-05 14:17:03 +1000

    Adds tests

---
+++ tests/test_addition.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_addition_one():
+    assert 1 + 1 == 2
+
+def test_addition_two_test():
+    assert 1 + 2 == 3
+
+def test_addition_three_test():
+    assert -5 + 3 == -2
+
+def test_addition_four_test():
+    assert 5 + 0 == 5
---
+++ tests/test_mult.py
@@ -0,0 +1,9 @@
+import pytest
+def test_mult_one():
+    assert 5 * 2 == 10
+
+def test_mult_two():
+    assert 10 * -3 == -30
+
+def test_mult_three():
+    assert -5 * -5 == 25
